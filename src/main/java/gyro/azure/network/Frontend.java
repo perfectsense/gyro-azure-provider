29a2754a10b2f1285a9bca266806f7cd7aaff969
Renee Adkins <reneeskate@aol.com>
2019-03-10 14:55:56 -0400

    Delete old fields, and add new ones

--- src/main/java/gyro/azure/network/Frontend.java
+++ src/main/java/gyro/azure/network/Frontend.java
@@ -2,64 +2,33 @@ package gyro.azure.network;
 
 import gyro.core.diff.Diffable;
 
+import java.util.List;
 
-    private Boolean staticPublicIp;
-    private String frontendName;
-    private String staticPublicIpAddress;
-    private String networkName;
-    private Boolean publicFrontEnd;
-    private String subnet;
 public abstract class Frontend extends Diffable {
 
-    public Boolean getStaticPublicIp() {
-        return staticPublicIp;
+    public List<InboundNatPool> inboundNatPool;
+    public List<InboundNatRule> inboundNatRule;
+
+    /**
+     * The inbound nat pools associated with the frontend. (Optional)
+     */
+    public List<InboundNatPool> getInboundNatPool() {
+        return inboundNatPool;
     }
 
-    public void setStaticPublicIp(Boolean staticPublicIp) {
-        this.staticPublicIp = staticPublicIp;
+    public void setInboundNatPool(List<InboundNatPool> inboundNatPool) {
+        this.inboundNatPool = inboundNatPool;
     }
 
-    public String getFrontendName() {
-        return frontendName;
+    /**
+     * The inbound nat rules associated with the frontend. Nat rules may not be
+     * associated with a frontend if a nat pool is associated. (Optional)
+     */
+    public List<InboundNatRule> getInboundNatRule() {
+        return inboundNatRule;
     }
 
-    public void setFrontendName(String frontendName) {
-        this.frontendName = frontendName;
-    }
-
-    public String getStaticPublicIpAddress() {
-        return staticPublicIpAddress;
-    }
-
-    public void setStaticPublicIpAddress(String staticPublicIpAddress) {
-        this.staticPublicIpAddress = staticPublicIpAddress;
-    }
-
-    public String getNetworkName() {
-        return networkName;
-    }
-
-    public void setNetworkName(String networkName) {
-        this.networkName = networkName;
-    }
-
-    public Boolean getPublicFrontEnd() {
-        return publicFrontEnd;
-    }
-
-    public void setPublicFrontEnd(Boolean publicFrontEnd) {
-        this.publicFrontEnd = publicFrontEnd;
-    }
-
-    public String getSubnet() {
-        return subnet;
-    }
-
-    public void setSubnet(String subnet) {
-        this.subnet = subnet;
-    }
-
-    }
-
+    public void setInboundNatRule(List<InboundNatRule> inboundNatRule) {
+        this.inboundNatRule = inboundNatRule;
     }
 }
