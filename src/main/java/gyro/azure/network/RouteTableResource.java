a393c7d361ec91f1538933484dedeb2d477e6282
Renee Adkins <reneeskate@aol.com>
2019-03-19 10:54:35 -0400

    Remove comment

--- src/main/java/gyro/azure/network/RouteTableResource.java
+++ src/main/java/gyro/azure/network/RouteTableResource.java
@@ -212,7 +212,6 @@ public class RouteTableResource extends AzureResource {
         List<Routes> subtractions = new ArrayList<>(currentResource.getRoutes());
         subtractions.removeAll(getRoutes());
 
-        //add and delete routes
         RouteTable.Update update = client.routeTables()
                 .getById(getId())
                 .update();
