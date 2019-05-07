2b52452ea9d1ebc4d4a5b972277cb903f8f5a0dc
Hyoo Lim <hyoolim@gmail.com>
2019-05-07 17:14:12 -0400

    Adds ResourceNamespace.

--- src/main/java/gyro/azure/package-info.java
+++ src/main/java/gyro/azure/package-info.java
@@ -3,6 +3,8 @@
  * ------------------
  */
 @DocNamespace("azure")
+@ResourceNamespace("azure")
 package gyro.azure;
 
 import gyro.core.resource.DocNamespace;
+import gyro.core.resource.ResourceNamespace;
