2760f6542c0837abfcce22bc2ff093b1cb96f07c
Renee Adkins <reneeskate@aol.com>
2019-05-14 18:18:04 -0400

    Implement cdn endpoint resource

---
+++ src/main/java/gyro/azure/cdn/CdnEndpointResource.java
@@ -0,0 +1,546 @@
+package gyro.azure.cdn;
+
+import gyro.azure.AzureResource;
+import gyro.core.GyroException;
+import gyro.core.resource.Resource;
+import gyro.core.resource.ResourceType;
+import gyro.core.resource.ResourceUpdatable;
+
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.cdn.CdnProfile;
+import com.microsoft.azure.management.cdn.CdnEndpoint;
+import com.microsoft.azure.management.cdn.QueryStringCachingBehavior;
+import com.microsoft.azure.management.cdn.CdnEndpoint.UpdateDefinitionStages.WithPremiumAttach;
+import com.microsoft.azure.management.cdn.CdnEndpoint.UpdateDefinitionStages.WithStandardAttach;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Creates a cdn endpoint.
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: gyro
+ *
+ *         azure::cdn-endpoint standard-cdn-endpoint-example
+ *             cdn-profile: $(azure::cdn-profile cdn-profile-example)
+ *             compression-enabled: false
+ *             content-types-to-compress: ["application/eot", "application/json", "text/html"]
+ *
+ *             geo-filter
+ *                 action: "ALLOW"
+ *                 country-codes: ["CA", "CL"]
+ *                 relative-path: "/"
+ *             end
+ *
+ *             geo-filter
+ *                 action: "BLOCK"
+ *                 country-codes: ["CA"]
+ *                 relative-path: "/relativepath"
+ *             end
+ *
+ *             host-header: "my.host.com"
+ *             http-allowed: true
+ *             http-port: 81
+ *             https-allowed: true
+ *             https-port: 8443
+ *             name: "test-endpoint-cdn"
+ *             origin-hostname: "origin.hostname.com"
+ *             origin-path: "/example/path"
+ *             query-caching-behavior: "BYPASS_CACHING"
+ *             type: "Standard"
+ *         end
+ */
+@ResourceType("cdn-endpoint")
+public class CdnEndpointResource extends AzureResource {
+
+    private static final String TYPE_PREMIUM = "Premium";
+    private static final String TYPE_STANDARD = "Standard";
+
+    private CdnProfileResource cdnProfile;
+    private Boolean compressionEnabled;
+    private Set<String> contentTypesToCompress;
+    private Set<String> customDomains;
+    private List<GeoFilter> geoFilter;
+    private String hostHeader;
+    private Boolean httpAllowed;
+    private Integer httpPort;
+    private Boolean httpsAllowed;
+    private Integer httpsPort;
+    private String name;
+    private String originHostname;
+    private String originPath;
+    private String queryCachingBehavior;
+    private Map<String, String> tags;
+    private String type;
+
+    /**
+     * The cdn profile resource where the endpoint is found. (Required)
+     */
+    public CdnProfileResource getCdnProfile() {
+        return cdnProfile;
+    }
+
+    public void setCdnProfile(CdnProfileResource cdnProfile) {
+        this.cdnProfile = cdnProfile;
+    }
+
+    /**
+     * Determines whether compression is enabled. (Optional)
+     */
+    @ResourceUpdatable
+    public Boolean getCompressionEnabled() {
+        return compressionEnabled;
+    }
+
+    public void setCompressionEnabled(Boolean compressionEnabled) {
+        this.compressionEnabled = compressionEnabled;
+    }
+
+    /**
+     * Determines what content types to be compressed. Used if compression is enabled. (Conditional)
+     */
+    @ResourceUpdatable
+    public Set<String> getContentTypesToCompress() {
+        if (contentTypesToCompress == null) {
+            contentTypesToCompress = new HashSet<>();
+        }
+
+        return contentTypesToCompress;
+    }
+
+    public void setContentTypesToCompress(Set<String> contentTypesToCompress) {
+        this.contentTypesToCompress = contentTypesToCompress;
+    }
+
+    /**
+     * Determines what custom domains are associated with the endpoint. (Optional)
+     */
+    @ResourceUpdatable
+    public Set<String> getCustomDomains() {
+        if (customDomains == null) {
+            customDomains = new HashSet<>();
+        }
+
+        return customDomains;
+    }
+
+    public void setCustomDomains(Set<String> customDomains) {
+        this.customDomains = customDomains;
+    }
+
+    /**
+     * The list of geo-filters associated with the endpoint. (Optional)
+     */
+    @ResourceUpdatable
+    public List<GeoFilter> getGeoFilter() {
+        if (geoFilter == null) {
+            geoFilter = new ArrayList<>();
+        }
+
+        return geoFilter;
+    }
+
+    public void setGeoFilter(List<GeoFilter> geoFilter) {
+        this.geoFilter = geoFilter;
+    }
+
+    /**
+     * The host header of the endpoint. (Optional)
+     */
+    @ResourceUpdatable
+    public String getHostHeader() {
+        return hostHeader;
+    }
+
+    public void setHostHeader(String hostHeader) {
+        this.hostHeader = hostHeader;
+    }
+
+    /**
+     * Determines whether http protocol is allowed. (Optional)
+     */
+    @ResourceUpdatable
+    public Boolean getHttpAllowed() {
+        return httpAllowed;
+    }
+
+    public void setHttpAllowed(Boolean httpAllowed) {
+        this.httpAllowed = httpAllowed;
+    }
+
+    /**
+     * The http port. Used if http port is allowed. (Conditional)
+     */
+    @ResourceUpdatable
+    public Integer getHttpPort() {
+        return httpPort;
+    }
+
+    public void setHttpPort(Integer httpPort) {
+        this.httpPort = httpPort;
+    }
+
+    /**
+     * Determines whether https protocol is allowed. (Optional)
+     */
+    @ResourceUpdatable
+    public Boolean getHttpsAllowed() {
+        return httpsAllowed;
+    }
+
+    public void setHttpsAllowed(Boolean httpsAllowed) {
+        this.httpsAllowed = httpsAllowed;
+    }
+
+    /**
+     * The https port. Used if https port is allowed. (Conditional)
+     */
+    @ResourceUpdatable
+    public Integer getHttpsPort() {
+        return httpsPort;
+    }
+
+    public void setHttpsPort(Integer httpsPort) {
+        this.httpsPort = httpsPort;
+    }
+
+    /**
+     * The origin path. (Optional)
+     */
+    @ResourceUpdatable
+    public String getOriginPath() {
+        return originPath;
+    }
+
+    public void setOriginPath(String originPath) {
+        this.originPath = originPath;
+    }
+
+    /**
+     * The name of the endpoint. (Required)
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The origin hostname. (Required)
+     */
+    public String getOriginHostname() {
+        return originHostname;
+    }
+
+    public void setOriginHostname(String originHostname) {
+        this.originHostname = originHostname;
+    }
+
+    /**
+     * Determines the query caching behavior. Values are ``IGNORE_QUERY_STRING``, ``BYPASS_CACHING``,
+     * and ``USE_QUERY_STRING``. (Optional)
+     */
+    @ResourceUpdatable
+    public String getQueryCachingBehavior() {
+        return queryCachingBehavior;
+    }
+
+    public void setQueryCachingBehavior(String queryCachingBehavior) {
+        this.queryCachingBehavior = queryCachingBehavior;
+    }
+
+    /**
+     * The tags associated with the endpoint. (Optional)
+     */
+    @ResourceUpdatable
+    public Map<String, String> getTags() {
+        if (tags == null) {
+            tags = new HashMap<>();
+        }
+
+        return tags;
+    }
+
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
+
+    /**
+     * The type of the endpoint. Values are Standard and Premium. (Required)
+     */
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public boolean refresh() {
+        Azure client = createClient();
+
+        CdnEndpoint cdnEndpoint = client.cdnProfiles().getById(getCdnProfile().getId()).endpoints().get(getName());
+        if (cdnEndpoint == null) {
+            return false;
+        }
+
+        setCompressionEnabled(cdnEndpoint.isCompressionEnabled());
+        setContentTypesToCompress(cdnEndpoint.contentTypesToCompress());
+        setCustomDomains(cdnEndpoint.customDomains());
+        cdnEndpoint.geoFilters().forEach(geo -> getGeoFilter().add(new GeoFilter(geo)));
+        setHostHeader(cdnEndpoint.originHostHeader());
+        setHttpAllowed(cdnEndpoint.isHttpAllowed());
+        setHttpPort(cdnEndpoint.httpPort());
+        setHttpsAllowed(cdnEndpoint.isHttpsAllowed());
+        setHttpsPort(cdnEndpoint.httpsPort());
+        setName(cdnEndpoint.name());
+        setOriginHostname(cdnEndpoint.originHostName());
+        setOriginPath(cdnEndpoint.originPath());
+        setQueryCachingBehavior(adjustQueueCaching(cdnEndpoint.queryStringCachingBehavior().toString()));
+        System.out.println("show qcb " + cdnEndpoint.queryStringCachingBehavior().toString());
+
+        return true;
+    }
+
+    @Override
+    public void create() {
+        Azure client = createClient();
+
+        CdnProfile cdnProfile = client.cdnProfiles().getById(getCdnProfile().getId());
+
+        if (TYPE_PREMIUM.equalsIgnoreCase(getType())) {
+            WithPremiumAttach<CdnProfile.Update> createPremiumEndpoint =
+                    cdnProfile.update()
+                    .defineNewPremiumEndpoint(getName(), getOriginHostname());
+
+            if (getHostHeader() != null) {
+                createPremiumEndpoint.withHostHeader(getHostHeader());
+            }
+
+            if (getHttpAllowed() != null && getHttpPort() != null) {
+                createPremiumEndpoint.
+                        withHttpAllowed(getHttpAllowed());
+                if (getHttpAllowed()) {
+                    createPremiumEndpoint.withHttpPort(getHttpPort());
+                }
+            }
+
+            if (getHttpsAllowed() != null && getHttpsPort() != null) {
+                createPremiumEndpoint
+                        .withHttpsAllowed(getHttpsAllowed());
+                if (getHttpsAllowed()) {
+                    createPremiumEndpoint.withHttpsPort(getHttpsPort());
+                }
+            }
+
+            if (getOriginPath() != null) {
+                createPremiumEndpoint.withOriginPath(getOriginPath());
+            }
+
+            for (String customDomain : getCustomDomains()) {
+                createPremiumEndpoint.withCustomDomain(customDomain);
+            }
+
+            createPremiumEndpoint.attach().apply();
+
+        } else if (TYPE_STANDARD.equalsIgnoreCase(getType())) {
+            WithStandardAttach<CdnProfile.Update> createStandardEndpoint =
+                    cdnProfile.update()
+                    .defineNewEndpoint(getName(), getOriginHostname());
+
+            if (getCompressionEnabled() != null && getContentTypesToCompress() != null) {
+                createStandardEndpoint
+                        .withCompressionEnabled(getCompressionEnabled())
+                        .withContentTypesToCompress(getContentTypesToCompress());
+            }
+
+            if (getGeoFilter() != null) {
+                createStandardEndpoint.withGeoFilters(toGeoFilters());
+            }
+
+            if (getHostHeader() != null) {
+                createStandardEndpoint.withHostHeader(getHostHeader());
+            }
+
+            if (getHttpAllowed() != null && getHttpPort() != null) {
+                createStandardEndpoint.
+                        withHttpAllowed(getHttpAllowed());
+                if (getHttpAllowed()) {
+                    createStandardEndpoint.withHttpPort(getHttpPort());
+                }
+            }
+
+            if (getHttpsAllowed() != null && getHttpsPort() != null) {
+                createStandardEndpoint
+                        .withHttpsAllowed(getHttpsAllowed());
+                if (getHttpsAllowed()) {
+                    createStandardEndpoint.withHttpsPort(getHttpsPort());
+                }
+            }
+
+            if (getOriginPath() != null) {
+                createStandardEndpoint.withOriginPath(getOriginPath());
+            }
+
+            if (getQueryCachingBehavior() != null) {
+                createStandardEndpoint
+                        .withQueryStringCachingBehavior(QueryStringCachingBehavior
+                                .valueOf(getQueryCachingBehavior()));
+            }
+
+            for (String customDomain : getCustomDomains()) {
+                createStandardEndpoint.withCustomDomain(customDomain);
+            }
+
+            createStandardEndpoint.attach().apply();
+
+        } else {
+            throw new GyroException("Invalid endpoint type. Valid values are Premium and Standard");
+        }
+    }
+
+    @Override
+    public void update(Resource current, Set<String> changedProperties) {
+        Azure client = createClient();
+
+        CdnProfile cdnProfile = client.cdnProfiles().getById(getCdnProfile().getId());
+
+        if (TYPE_PREMIUM.equalsIgnoreCase(getType())) {
+            CdnEndpoint.UpdatePremiumEndpoint updatePremiumEndpoint =
+                    cdnProfile
+                    .update().updatePremiumEndpoint(getName());
+
+            if (getHostHeader() != null) {
+                updatePremiumEndpoint.withHostHeader(getHostHeader());
+            }
+
+            if (getHttpAllowed() != null && getHttpPort() != null) {
+                updatePremiumEndpoint.
+                        withHttpAllowed(getHttpAllowed());
+                if (getHttpAllowed()) {
+                    updatePremiumEndpoint.withHttpPort(getHttpPort());
+                }
+            }
+
+            if (getHttpsAllowed() != null && getHttpsPort() != null) {
+                updatePremiumEndpoint
+                        .withHttpsAllowed(getHttpsAllowed());
+                if (getHttpsAllowed()) {
+                    updatePremiumEndpoint.withHttpsPort(getHttpsPort());
+                }
+            }
+
+            if (getOriginPath() != null) {
+                updatePremiumEndpoint.withOriginPath(getOriginPath());
+            }
+
+            for (String customDomain : getCustomDomains()) {
+                updatePremiumEndpoint.withCustomDomain(customDomain);
+            }
+
+            updatePremiumEndpoint.parent().apply();
+
+        } else if (TYPE_STANDARD.equalsIgnoreCase(getType())) {
+            CdnEndpoint.UpdateStandardEndpoint updateStandardEndpoint =
+                    cdnProfile
+                    .update().updateEndpoint(getName());
+
+            if (getCompressionEnabled() != null && getContentTypesToCompress() != null) {
+                updateStandardEndpoint
+                        .withCompressionEnabled(getCompressionEnabled())
+                        .withContentTypesToCompress(getContentTypesToCompress());
+            }
+
+            if (getGeoFilter() != null) {
+                updateStandardEndpoint.withGeoFilters(toGeoFilters());
+            }
+
+            if (getHostHeader() != null) {
+                updateStandardEndpoint.withHostHeader(getHostHeader());
+            }
+
+            if (getHttpAllowed() != null && getHttpPort() != null) {
+                updateStandardEndpoint.
+                        withHttpAllowed(getHttpAllowed());
+                if (getHttpAllowed()) {
+                    updateStandardEndpoint.withHttpPort(getHttpPort());
+                }
+            }
+
+            if (getHttpsAllowed() != null && getHttpsPort() != null) {
+                updateStandardEndpoint
+                        .withHttpsAllowed(getHttpsAllowed());
+                if (getHttpsAllowed()) {
+                    updateStandardEndpoint.withHttpsPort(getHttpsPort());
+                }
+            }
+
+            if (getOriginPath() != null) {
+                updateStandardEndpoint.withOriginPath(getOriginPath());
+            }
+
+            if (getQueryCachingBehavior() != null) {
+                updateStandardEndpoint
+                        .withQueryStringCachingBehavior(QueryStringCachingBehavior
+                        .valueOf(getQueryCachingBehavior()));
+
+                System.out.println("show query caching " + QueryStringCachingBehavior
+                        .valueOf(getQueryCachingBehavior()));
+
+            }
+
+            for (String customDomain : getCustomDomains()) {
+                updateStandardEndpoint.withCustomDomain(customDomain);
+            }
+
+            updateStandardEndpoint.parent().apply();
+        }
+    }
+
+    @Override
+    public void delete() {
+        Azure client = createClient();
+
+        CdnProfile cdnProfile = client.cdnProfiles().getById(getCdnProfile().getId());
+
+        cdnProfile.update().withoutEndpoint(getName()).apply();
+    }
+
+    @Override
+    public String toDisplayString() {
+        return getType().toLowerCase() + " cdn endpoint " + getName();
+    }
+
+    private List<com.microsoft.azure.management.cdn.GeoFilter> toGeoFilters() {
+        List<com.microsoft.azure.management.cdn.GeoFilter> geoFilters = new ArrayList<>();
+
+        getGeoFilter().forEach(geo -> geoFilters.add(geo.toGeoFilter()));
+
+        return geoFilters;
+    }
+
+    private String adjustQueueCaching(String behavior) {
+        if ("BypassCaching".equals(behavior)) {
+            return "BYPASS_CACHING";
+        } else if ("IgnoreQueryString".equals(behavior)) {
+            return "IGNORE_QUERY_STRING";
+        } else if ("NotSet".equals(behavior)) {
+            return "NOT_SET";
+        } else if ("UseQueryString".equals(behavior)) {
+            return "USE_QUERY_STRING";
+        }
+
+        return null;
+    }
+}
