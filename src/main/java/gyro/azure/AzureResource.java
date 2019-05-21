7846a42d28d3bd7b4fd2619b346f243ef05a50f0
Hyoo Lim <hyoolim@gmail.com>
2019-05-21 14:03:25 -0400

    Moves createClient logic into AzureCredentials.

--- src/main/java/gyro/azure/AzureResource.java
+++ src/main/java/gyro/azure/AzureResource.java
@@ -1,33 +1,16 @@
 package gyro.azure;
 
-import gyro.core.GyroException;
 import gyro.core.resource.Resource;
 import com.microsoft.azure.management.Azure;
 
-import java.io.IOException;
-
 public abstract class AzureResource extends Resource {
-    private Azure client;
 
     protected Azure createClient() {
-        if (client == null) {
-            AzureCredentials azureCredentials = (AzureCredentials) credentials();
-            Azure.Authenticated authenticated = azureCredentials.findCredentials(true);
-
-            try {
-                if (authenticated != null) {
-                    client = authenticated.withDefaultSubscription();
-                }
-            } catch (IOException ex) {
-                throw new GyroException(ex.getMessage(), ex);
-            }
-        }
-
-        return client;
+        return ((AzureCredentials) credentials()).createClient();
     }
 
     protected String getRegion() {
-        AzureCredentials azureCredentials = (AzureCredentials) credentials();
-        return azureCredentials.getRegion();
+        return ((AzureCredentials) credentials()).getRegion();
     }
+
 }
