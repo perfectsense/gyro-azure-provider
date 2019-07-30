8b9f5b878fd12b9e49ca5be0b3ce96cbcd42e276
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-07-30 10:45:09 -0400

    Add Disk resource finder.

---
+++ src/main/java/gyro/azure/compute/DiskResourceFinder.java
@@ -0,0 +1,48 @@
+package gyro.azure.compute;
+
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.compute.Disk;
+import gyro.azure.AzureFinder;
+import gyro.core.Type;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Query Disk.
+ *
+ * .. code-block:: gyro
+ *
+ *    disk: $(external-query azure::disk {} )
+ */
+@Type("disk")
+public class DiskResourceFinder extends AzureFinder<Disk, DiskResource> {
+    private String id;
+
+    /**
+     * The ID of the Disk.
+     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    protected List<Disk> findAllAzure(Azure client) {
+        return client.disks().list();
+    }
+
+    @Override
+    protected List<Disk> findAzure(Azure client, Map<String, String> filters) {
+        Disk disk = client.disks().getById(filters.get("id"));
+        if (disk == null) {
+            return Collections.emptyList();
+        } else {
+            return Collections.singletonList(disk);
+        }
+    }
+}
