4e6201ecf9c6d13e2b22371ac1ec4441ba90d626
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-07-30 16:31:23 -0400

    Add availability set resource finder.

---
+++ src/main/java/gyro/azure/compute/AvailabilitySetResourceFinder.java
@@ -0,0 +1,47 @@
+package gyro.azure.compute;
+
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.compute.AvailabilitySet;
+import gyro.azure.AzureFinder;
+import gyro.core.Type;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Query Availability Set.
+ *
+ * .. code-block:: gyro
+ *
+ *    availability-set: $(external-query azure::availability-set {} )
+ */
+@Type("availability-set")
+public class AvailabilitySetResourceFinder extends AzureFinder<AvailabilitySet, AvailabilitySetResource> {
+    private String id;
+
+    /**
+     * The ID of the Availability Set.
+     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    @Override
+    protected List<AvailabilitySet> findAllAzure(Azure client) {
+        return client.availabilitySets().list();
+    }
+
+    @Override
+    protected List<AvailabilitySet> findAzure(Azure client, Map<String, String> filters) {
+        AvailabilitySet availabilitySet = client.availabilitySets().getById(filters.get("id"));
+        if (availabilitySet == null) {
+            return Collections.emptyList();
+        } else {
+            return Collections.singletonList(availabilitySet);
+        }
+    }
+}
