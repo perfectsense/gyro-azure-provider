32133ba411ee79ce8ce06e754f6480216c3d7e7a
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-08-18 23:25:19 -0400

    Refactor cloud file
    Implement Copyable.
    Implement Direct Dependency.
    Update Docs.
    Add validation annotation.

--- src/main/java/gyro/azure/storage/CloudFileShareResource.java
+++ src/main/java/gyro/azure/storage/CloudFileShareResource.java
@@ -1,6 +1,7 @@
 package gyro.azure.storage;
 
 import gyro.azure.AzureResource;
+import gyro.azure.Copyable;
 import gyro.core.GyroException;
 import gyro.core.GyroUI;
 import gyro.core.resource.Updatable;
@@ -13,6 +14,7 @@ import com.microsoft.azure.storage.file.FileShareProperties;
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.StorageException;
 import gyro.core.scope.State;
+import gyro.core.validation.Required;
 
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
@@ -27,27 +29,28 @@ import java.util.Set;
  * .. code-block:: gyro
  *
  *     azure::cloud-file-share cloud-file-share-example
- *         cloud-file-share-name: "example-cloud-file-share"
+ *         name: "example-cloud-file-share"
  *         share-quota: 10
  *         storage-account: $(azure::storage-account blob-storage-account-example)
  *     end
  */
 @Type("cloud-file-share")
-public class CloudFileShareResource extends AzureResource {
+public class CloudFileShareResource extends AzureResource implements Copyable<CloudFileShare> {
 
-    private String cloudFileShareName;
+    private String name;
     private Integer shareQuota;
     private StorageAccountResource storageAccount;
 
     /**
-     * The name of the cloud share. (Required)
+     * The name of the Cloud Share. (Required)
      */
-    public String getCloudFileShareName() {
-        return cloudFileShareName;
+    @Required
+    public String getName() {
+        return name;
     }
 
-    public void setCloudFileShareName(String cloudFileShareName) {
-        this.cloudFileShareName = cloudFileShareName;
+    public void setName(String name) {
+        this.name = name;
     }
 
     /**
@@ -63,8 +66,9 @@ public class CloudFileShareResource extends AzureResource {
     }
 
     /**
-     * The storage account resource where the file share will be created. (Required)
+     * The Storage Account where the file share will be created. (Required)
      */
+    @Required
     public StorageAccountResource getStorageAccount() {
         return storageAccount;
     }
@@ -74,15 +78,23 @@ public class CloudFileShareResource extends AzureResource {
     }
 
     @Override
+    public void copyFrom(CloudFileShare share) {
+        setName(share.getName());
+        setShareQuota(share.getProperties().getShareQuota());
+        setStorageAccount(findById(StorageAccountResource.class, share.getStorageUri().getPrimaryUri().getAuthority().split(".file.core")[0]));
+    }
+
+    @Override
     public boolean refresh() {
         try {
             CloudFileShare share = cloudFileShare();
-            if (share.exists()) {
-                setCloudFileShareName(share.getName());
-                setShareQuota(share.getProperties().getShareQuota());
-                return true;
+            if (!share.exists()) {
+                return false;
             }
-            return false;
+
+            copyFrom(share);
+
+            return true;
         } catch (StorageException ex) {
             throw new GyroException(ex.getMessage());
         }
@@ -123,7 +135,7 @@ public class CloudFileShareResource extends AzureResource {
         try {
             CloudStorageAccount storageAccount = CloudStorageAccount.parse(getStorageAccount().getConnection());
             CloudFileClient fileClient = storageAccount.createCloudFileClient();
-            return fileClient.getShareReference(getCloudFileShareName());
+            return fileClient.getShareReference(getName());
         } catch (StorageException | URISyntaxException | InvalidKeyException ex) {
             throw new GyroException(ex.getMessage());
         }
