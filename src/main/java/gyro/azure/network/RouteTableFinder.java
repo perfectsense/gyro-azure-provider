6d945ad41342fdb3ab99cf0ec015c3ee7e434eba
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-08-20 11:00:14 -0400

    Add Route Table Finder.

---
+++ src/main/java/gyro/azure/network/RouteTableFinder.java
@@ -0,0 +1,40 @@
+package gyro.azure.network;
+
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.network.RouteTable;
+import gyro.azure.AzureFinder;
+import gyro.core.Type;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Type("route-table")
+public class RouteTableFinder extends AzureFinder<RouteTable, RouteTableResource> {
+    private String id;
+
+    /**
+     * The ID of the Route Table.
+     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    @Override
+    protected List<RouteTable> findAllAzure(Azure client) {
+        return client.routeTables().list();
+    }
+
+    @Override
+    protected List<RouteTable> findAzure(Azure client, Map<String, String> filters) {
+        RouteTable routeTable = client.routeTables().getById(filters.get("id"));
+        if (routeTable == null) {
+            return Collections.emptyList();
+        } else {
+            return Collections.singletonList(routeTable);
+        }
+    }
+}
