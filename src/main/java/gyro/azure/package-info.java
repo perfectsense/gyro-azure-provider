0d02081a798346df8920512234e867ab20c9e895
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-10-09 09:01:11 -0400

    Remove extra * from provider docs

--- src/main/java/gyro/azure/package-info.java
+++ src/main/java/gyro/azure/package-info.java
@@ -5,16 +5,16 @@
  * The Azure provider implements support for Azure cloud provider.
  *
  * Usage
- *  * +++++
- *  *
- *  * The Azure provider is implemented as a plugin. To use it add the plugin to your init file.
- *  * It uses the format ``@plugin: gyro:gyro-azure-provider:<version>``.
- *  *
- *  * .. code:: shell
- *  *
- *  *     {@literal @}plugin: 'gyro:gyro-azure-provider:0.99.0-SNAPSHOT'
- *  *
- *  * This lets Gyro load the Azure provider plugin and lets you start managing Azure resources using Gyro.
+ * +++++
+ *
+ * The Azure provider is implemented as a plugin. To use it add the plugin to your init file.
+ * It uses the format ``@plugin: gyro:gyro-azure-provider:<version>``.
+ *
+ * .. code:: shell
+ *
+ *     {@literal @}plugin: 'gyro:gyro-azure-provider:0.99.0-SNAPSHOT'
+ *
+ * This lets Gyro load the Azure provider plugin and lets you start managing Azure resources using Gyro.
  *
  * Authentication
  * ++++++++++++++
