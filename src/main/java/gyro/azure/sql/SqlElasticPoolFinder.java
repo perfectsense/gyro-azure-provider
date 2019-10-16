11294368c4805f4103b8664538c6995d9647ae3e
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-10-16 16:12:12 -0400

    Add sql database finder

---
+++ src/main/java/gyro/azure/sql/SqlElasticPoolFinder.java
@@ -0,0 +1,70 @@
+package gyro.azure.sql;
+
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.sql.SqlElasticPool;
+import com.microsoft.azure.management.sql.SqlServer;
+import gyro.azure.AzureFinder;
+import gyro.core.Type;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Query sql elastic pool.
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: gyro
+ *
+ *    sql-elastic-pool: $(external-query azure::sql-elastic-pool {})
+ */
+@Type("sql-elastic-pool")
+public class SqlElasticPoolFinder extends AzureFinder<SqlElasticPool, SqlElasticPoolResource> {
+    private String sqlServerId;
+    private String name;
+
+    /**
+     * The ID of the SQL Server.
+     */
+    public String getSqlServerId() {
+        return sqlServerId;
+    }
+
+    public void setSqlServerId(String sqlServerId) {
+        this.sqlServerId = sqlServerId;
+    }
+
+    /**
+     * The name of the Elastic Pool.
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected List<SqlElasticPool> findAllAzure(Azure client) {
+        return client.sqlServers().list().stream().map(o -> o.elasticPools().list()).flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
+    @Override
+    protected List<SqlElasticPool> findAzure(Azure client, Map<String, String> filters) {
+        SqlServer sqlServer = client.sqlServers().getById(filters.get("sql-server-id"));
+        if (sqlServer != null) {
+            if (filters.containsKey("name")) {
+                return Collections.singletonList(sqlServer.elasticPools().get(filters.get("name")));
+            } else {
+                return sqlServer.elasticPools().list();
+            }
+        } else {
+            return Collections.emptyList();
+        }
+    }
+}
