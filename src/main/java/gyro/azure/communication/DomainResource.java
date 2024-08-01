579652574ca3223a73d970b66735cea2508ac3a4
Harshil Sanjay Jain <harshil.jain@brightspot.com>
2024-08-01 13:43:13 -0400

    Update licenses

--- src/main/java/gyro/azure/communication/DomainResource.java
+++ src/main/java/gyro/azure/communication/DomainResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, Perfect Sense, Inc.
+ * Copyright 2024, Brightspot, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
